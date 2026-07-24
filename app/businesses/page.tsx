a606c7dfdeb553eef9a286eed501198b20514610
eyiden1 <eyiden1@wgu.edu>
2026-07-24 11:03:05 -0400

    QA-

--- app/businesses/page.tsx
+++ app/businesses/page.tsx
@@ -2,6 +2,9 @@ import Link from "next/link";
 import BusinessList from "@/components/businesses/BusinessList";
 import { getBusinesses } from "@/lib/businesses/queries";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default async function BusinessesPage() {
   const businesses = await getBusinesses();
 
